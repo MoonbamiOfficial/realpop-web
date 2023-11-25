03510c66958bee14110dd65e84b31ed81cd4e440
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-11-26 00:50:43 +0800

    feat: auto center home page

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -15,7 +15,7 @@ const Home = (props: Props) => {
     <>
       <BgVideo />
       <Gradient />
-      <section id="home" className="container relative top-[400px] h-full">
+      <section id="home" className="container mx-auto relative top-[400px] h-full">
         <div className="ads w-[400px] flex flex-col items-end gap-y-[50px] ml-[40px] sm:w-[500px] sm:ml-[50px] sm:top-[350px] lg:top-[325px] lg:mx-0 lg:ml-[75px] xl:ml-[100px] 2xl:ml-[150px] premium:w-[600px] premium:ml-[200px] ">
           <div className="text relative flex flex-col justify-end gap-y-[8px]">
             <h1 className="title capitalize text-[2.5rem] font-[700] leading-[3rem] tracking-tight sm:text-[3.125rem] sm:leading-[3.75rem] md:text-[4rem] md:leading-[4.8rem] 2xl:text-[4.875rem] 2xl:leading-[5.85rem] premium:text-[6rem] premium:leading-[7.2rem]">saranghae!</h1>
