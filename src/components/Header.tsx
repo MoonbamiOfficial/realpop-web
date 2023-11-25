3a4b80ef5cfbaea4e86db1e8199a63e8c4a8c426
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-11-25 23:43:23 +0800

    feat: change header to fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -33,8 +33,8 @@ const Header = () => {
 
 
       {/* Left */}
-      <div id={ headerBg } className="header-bg fixed top-0 bg-opaque-black w-screen h-[60px] sm:h-[80px] "></div>
-      <header id="" className="container mx-auto relative z-50 w-full h-[60px] p-[15px] flex justify-between items-center sm:px-[25px] sm:h-[80px] md:px-[50px] lg:px-[75px] xl:px-[100px] 2xl:px-[150px] premium:px-[200px]">
+      <div id={ headerBg } className="header-bg fixed top-0 z-50 bg-opaque-black w-screen h-[60px] sm:h-[80px] "></div>
+      <header id="" className="container mx-auto fixed left-[50%] z-50 w-full h-[60px] flex justify-between items-center px-[15px] sm:px-[25px] sm:h-[80px] md:px-[50px] lg:px-[75px] xl:px-[100px] 2xl:px-[150px] premium:px-[200px]">
         <div id="left" className=" flex items-center gap-x-[8px] w-[115px] md:w-[130px] lg:w-[140px] lg:gap-x-[32px] 2xl:w-[160px]">
           <button onClick={ e => {
             setIsDropdownActive(!isDropdownActive)
@@ -51,10 +51,10 @@ const Header = () => {
               <div className="dropdown z-50 flex items-start fixed top-[60px]  h-[150px] sm:top-[80px] lg:hidden ">
                 <div className="flex flex-col gap-y-[6px] ">
                   <NavLink to="" className="burger-nav ">Home</NavLink>
-                  <NavLink to="" className="burger-nav ">About</NavLink>
-                  <NavLink to="" className="burger-nav ">Shop</NavLink>
-                  <NavLink to="" className="burger-nav ">Contact</NavLink>
-                  <NavLink to="" className="burger-nav ">Help</NavLink>
+                  <NavLink to="about" className="burger-nav ">About</NavLink>
+                  <NavLink to="shop" className="burger-nav ">Shop</NavLink>
+                  <NavLink to="contact" className="burger-nav ">Contact</NavLink>
+                  <NavLink to="help" className="burger-nav ">Help</NavLink>
                 </div>
             </div>
           </>
@@ -71,10 +71,10 @@ const Header = () => {
           <img src={ Realpop_Logo } alt="Realpop Logo" className="w-[50px] h-[50px] sm:w-[60px] sm:h-[60px] lg:hidden cursor-pointer" />
           <div className="main-nav-container hidden lg:flex lg:gap-x-1">
             <NavLink to="" className="main-nav ">Home</NavLink>
-            <NavLink to="" className="main-nav ">About</NavLink>
-            <NavLink to="" className="main-nav ">Shop</NavLink>
-            <NavLink to="" className="main-nav ">Contact</NavLink>
-            <NavLink to="" className="main-nav ">Help</NavLink>
+            <NavLink to="about" className="main-nav ">About</NavLink>
+            <NavLink to="shop" className="main-nav ">Shop</NavLink>
+            <NavLink to="contact" className="main-nav ">Contact</NavLink>
+            <NavLink to="help" className="main-nav ">Help</NavLink>
           </div>
         </div>
 
@@ -93,9 +93,9 @@ const Header = () => {
             else if(!isDropdownActive) setIsDropdownActive(isDropdownActive)
             else setIsDropdownActive(false)
             }} className="user-btn lg:w-[60px] lg:h-[60px]">
-            <Link to="" className="lg:hidden">
+            <div className="lg:hidden">
               <i className="fa-solid fa-user"></i>
-            </Link>
+            </div>
             <Link to="" className="user-icon hidden w-[60px] h-[60px] border-[3px] border-white rounded-full lg:block "></Link>
           </button>
           {/* Dropdown for user menu */}
@@ -104,8 +104,8 @@ const Header = () => {
               <div className="dropdown-bg fixed top-[60px] left-0 w-screen h-[180px] bg-opaque-black border-electric-pink border-b-[5px] sm:top-[80px] lg:hidden"></div>
               <div className="dropdown  z-50 flex items-start fixed top-[60px]  h-[150px] sm:top-[80px] lg:hidden ">
                 <div className="flex flex-col gap-y-[6px] ">
-                  <NavLink to="" className="user-nav ">Profile</NavLink>
-                  <NavLink to="" className="user-nav ">Cart</NavLink>
+                  <NavLink to="profile" className="user-nav ">Profile</NavLink>
+                  <NavLink to="cart" className="user-nav ">Cart</NavLink>
                 </div>
             </div>
           </>
