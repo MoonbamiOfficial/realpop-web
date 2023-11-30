39fc783230d1194ec9588184a44e2ebd1a17ab26
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-11-30 16:34:49 +0800

    feat: switch user nav links order

--- src/components/header/UserMenu.tsx
+++ src/components/header/UserMenu.tsx
@@ -7,8 +7,8 @@ const UserMenu = (props: Props) => {
   return (
     <div className="dropdown  z-[100] flex items-start absolute top-[60px]  h-[150px] sm:top-[80px] lg:hidden ">
       <div className="flex flex-col gap-y-[6px] ">
-        <NavLink to="profile" className="user-nav ">Profile</NavLink>
         <NavLink to="cart" className="user-nav ">Cart</NavLink>
+        <NavLink to="profile" className="user-nav ">Profile</NavLink>
       </div>
     </div>
   )
