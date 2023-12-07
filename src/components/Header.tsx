576bbc29aa862d1117d98162c0c821b796b5129e
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-12-07 15:30:51 +0800

    feat: new header style for md screen below

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,18 +43,18 @@ const Header = () => {
 
   return (
     <>
-      <div className="header-bg top-0 fixed z-[100] bg-opaque-black w-full h-[60px] sm:h-[80px] "></div>
+      <div className="header-bg top-0 fixed z-[100] bg-header-black w-full h-[60px] sm:h-[80px] "></div>
       <header ref={ dropdown } id="" className="container fixed left-[50%] translate-x-[-50%] top-0 z-[100] w-full h-[60px] flex justify-between items-center px-[15px] sm:px-[25px] sm:h-[80px] md:px-[50px] lg:px-[75px] xl:px-[100px] 2xl:px-[150px] premium:px-[200px]">
 
 
 
         {/* Left */}
-        <div id="left" className=" flex items-center gap-x-[8px] w-[115px] md:w-[130px] lg:w-[140px] lg:gap-x-[32px] 2xl:w-[160px]">
-          <button onClick={ toggleBurgerDropdown } className="burger-menu w-[50px] h-[50px] text-[24px] sm:text-[32px] md:text-[36px] lg:hidden">
+        <div id="left" className=" flex items-center w-[115px] md:w-[130px] lg:w-[140px] lg:gap-x-[15px] 2xl:w-[160px]">
+          <button onClick={ toggleBurgerDropdown } className="burger-menu w-[50px] h-[50px] text-[1.5rem] sm:text-[2rem] md:text-[2.25rem] lg:hidden">
             <i className="fa-solid fa-bars"></i>
           </button>
-          <div className={`${isBurgerDropdownActive? 'dropdownActive' : 'dropdownInactive'} z-[100] absolute top-[60px] h-[150px] sm:top-[80px] lg:hidden `}>
-            <div className="flex flex-col gap-y-[6px] ">
+          <div className={`${isBurgerDropdownActive? 'dropdownActive' : 'dropdownInactive'} z-[100] absolute top-[70px] h-[150px] sm:top-[90px] md:top-[100px] lg:hidden `}>
+            <div className="flex flex-col gap-y-[6px] md:gap-y-[12px] ">
               <BurgerDropdown to={"/"} text={"Home"} />
               <BurgerDropdown to={"/about"} text={"About"} />
               <BurgerDropdown to={"/shop"} text={"Shop"} />
@@ -62,9 +62,9 @@ const Header = () => {
               <BurgerDropdown to={"/help"} text={"Help"} />
             </div>
           </div>
-          <img src={ Realpop_Logo } alt="Realpop Logo" className="w-[60px] h-[60px] hidden lg:block"/>
-          <Link to="/" className="hidden text-[1.5rem] font-[700] capitalize lg:block xl:hidden">Rp</Link>
-          <Link to="/" className="hidden text-[2rem] font-[700] capitalize xl:block">Realpop</Link>
+          <img src={ Realpop_Logo } alt="Realpop Logo" className="w-[50px] h-[50px] hidden lg:block"/>
+          <Link to="/" className="hidden text-[2rem] font-[700] capitalize lg:block xl:hidden">Rp</Link>
+          <Link to="/" className="hidden text-[1.75rem] font-[700] capitalize xl:block">Realpop</Link>
         </div>
 
 
@@ -72,7 +72,7 @@ const Header = () => {
         {/* Mid */}
         <div id="mid" className="">
           <Link to="/">
-            <img src={ Realpop_Logo } alt="Realpop Logo" className="w-[50px] h-[50px] sm:w-[60px] sm:h-[60px] lg:hidden" />
+            <img src={ Realpop_Logo } alt="Realpop Logo" className="w-[40px] h-[40px] sm:w-[60px] sm:h-[60px] lg:hidden" />
           </Link>
           <div className="main-nav-container hidden lg:flex lg:gap-x-1">
             <NavLink to="/" className="main-nav ">Home</NavLink>
@@ -86,23 +86,21 @@ const Header = () => {
 
 
         {/* Right */}
-        <div id="right" className=" flex justify-end items-center gap-x-[15px] w-[115px] md:w-[130px] md:gap-x-[30px] lg:w-[140px] 2xl:w-[160px] 2xl:gap-x-[50px]">
+        <div id="right" className=" flex justify-end items-center w-[115px] md:w-[130px] lg:gap-x-[15px] lg:w-[140px] xl:gap-x-[30px] 2xl:w-[160px] ">
           <NavLink to="/cart" className="user-cart hidden items-center justify-center rounded-[4px] hover:bg-electric-pink transition-all lg:flex lg:text-[24px]">
             <i className="fa-solid fa-cart-shopping"></i>
           </NavLink>
           <Link to="/profile" className="user-icon hidden overflow-hidden w-[60px] h-[60px] border-[3px] border-transparent rounded-full hover:border-electric-pink transition-all lg:block ">
             <img src={ Admin_Jam } alt="" />
           </Link>
-          <button onClick={ toggleUserDropdown } className="user-btn lg:hidden lg:w-[60px] lg:h-[60px]">
-            <div className="">
-              <i className="fa-solid fa-user"></i>
-            </div>
+          <button onClick={ toggleUserDropdown } className="user-btn text-[1.25rem] sm:text-[1.5rem] md:text-[1.75rem] lg:hidden lg:w-[60px] lg:h-[60px]">
+            <i className="fa-solid fa-user"></i>
           </button>
           {/* Dropdown for user menu */}
-          <div className={`${isUserDropdownActive? 'dropdownActive' : 'dropdownInactive'} z-[100] absolute top-[60px]  h-[150px] sm:top-[80px] lg:hidden`}>
-            <div className="flex flex-col gap-y-[6px] ">
-              <UserDropdown to={"/cart"} text={"Cart"} />
+          <div className={`${isUserDropdownActive? 'dropdownActive' : 'dropdownInactive'} z-[100] absolute top-[70px] h-[150px] sm:top-[90px] md:top-[100px] lg:hidden`}>
+            <div className="flex flex-col gap-y-[6px] md:gap-y-[12px] ">
               <UserDropdown to={"/profile"} text={"Profile"} />
+              <UserDropdown to={"/cart"} text={"Cart"} />
             </div>
           </div>
         </div>
