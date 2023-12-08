201634871531a17e6e4d62f1efdedc9b508dc00d
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-12-08 22:56:27 +0800

    feat: create route for girlgroup pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { 
   createBrowserRouter, 
   createRoutesFromElements,
@@ -17,6 +18,16 @@ import Help from './pages/help/page'
 import Cart from './pages/cart/page'
 import Profile from './pages/profile/page'
 
+// Sub-Pages
+import Twice from './pages/shop/twice/page'
+import Nmixx from './pages/shop/nmixx/page'
+import NewJeans from './pages/shop/new-jeans/page'
+import Aespa from './pages/shop/aespa/page'
+import LeSserafim from './pages/shop/le-sserafim/page'
+
+// Error Pages
+import NotFound from './pages/not-found/page'
+
 type Props = {}
 
 const router = createBrowserRouter(
@@ -24,11 +35,20 @@ const router = createBrowserRouter(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About /> } />
-      <Route path="shop" element={<Shop />} />
+      <Route path="shop" element={<Shop />} >
+        <Route path="TWICE" element={<Twice /> } />
+        <Route path="NMIXX" element={<Nmixx /> } />
+        <Route path="NewJeans" element={<NewJeans /> } />
+        <Route path="aespa" element={<Aespa /> } />
+        <Route path="LESSERAFIM" element={<LeSserafim /> } />
+      </Route>
       <Route path="contact" element={<Contact />} />
       <Route path="help" element={<Help />} />
       <Route path="cart" element={<Cart />} />
       <Route path="profile" element={<Profile />} />
+
+      {/* 404 */}
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 )
