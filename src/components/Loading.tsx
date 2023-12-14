bbc6d7ff3d553847efc1af1a43b6dc98a4f9038f
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-12-14 22:40:56 +0800

    feat: add new loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+type Props = {}
+
+const NotFound = () => {
+  return (
+    <>
+      <h1 className="text-[2.5rem] font-[500] capitalize">Loading...</h1>
+    </>
+  )
+}
+
+export default NotFound
