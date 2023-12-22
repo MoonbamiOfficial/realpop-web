328669cf131ad89391ffa70b2792512b9526592c
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-12-22 16:27:49 +0800

    feat: add z index position to loading for home

--- src/components/HomeBackgroundLoading.tsx
+++ src/components/HomeBackgroundLoading.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 
 // Assets
-import Realpop from '../assets/images/icon/realpop-official-logo.png'
-
-type Props = {}
+import Realpop from '../assets/images/logo/realpop-official-logo.png'
 
 const HomeBackgroundLoading = () => {
   return (
     <>
-      <div className=" absolute left-[50%] translate-x-[-50%]  flex justify-center items-center gap-2 animate-pulse transition-all " >
+      <div className=" absolute -z-[100] left-[50%] translate-x-[-50%]  flex justify-center items-center gap-2 animate-pulse transition-all " >
         <img src={ Realpop } alt="" className=" w-[500px] h-full " />
       </div>
     </>
